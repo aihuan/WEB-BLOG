7fee3c099e0d0ea66ca9e2849d8405c2f8f90cc6
liukekeone <1262815351@qq.com>
2021-08-05 19:12:47 +0800

    对象

---
+++ 对象.js
@@ -0,0 +1,8 @@
+// 对象是用来描述的 人或物
+var xiaohong = {
+        name: 'xiaohong',
+        'middle-school': 'hope school',
+    }
+    // 因为 middle-school 不是一个有效的变量 所以不可以直接用xiaohong.middle-school引用
+
+console.log(xiaohong['middle-school']);
