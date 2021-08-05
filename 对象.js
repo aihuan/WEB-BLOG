9fd98f5b3273d3cee13c273c7527debed37136ab
liukekeone <1262815351@qq.com>
2021-08-05 19:42:33 +0800

    Map/Set

--- 对象.js
+++ 对象.js
@@ -31,3 +31,23 @@ for (var key in o.hasOwnProperty()) {
 
 // !!特别要注意的是，我们对array 进行for in 时，得到的是String，而不是Number 切记切尔。
 // while |for |do while do while至少会执行一次
+
+/*===============Map Set==================*/
+// ES6引用新的数据类型  因为JS中的对象都是键值对的形式，然而数值却不能作为键值对，所以引用了支持的
+var m = new Set()
+var n = new Map()
+
+var m = new Map([
+    ['Michael', 95],
+    ['Bob', 75],
+    ['Tracy', 85]
+]);
+m.get('Michael'); // 95
+
+var stu = new Map();
+stu.set('lkk', 99);
+console.log(stu); //不能多次给一个Map对象赋值 那么后面的会覆盖前面的
+// Set 给定一个Array 如果重复了，那么会自动清除重复的值
+var num = new Set([1, 3, 4, 3, 5, 6])
+num.add(9) //(5)就没有效果的 delete 删除
+console.log(num); //可以重复添加，但不会有效果，用add添加
